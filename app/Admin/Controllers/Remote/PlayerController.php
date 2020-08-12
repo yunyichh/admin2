710126adfaea8ff0f89cf7a6f806f5891f1eb15b
unknown <yunyichh@outlook.com>
2020-08-12 18:16:06 +0800

    1

--- app/Admin/Controllers/Remote/PlayerController.php
+++ app/Admin/Controllers/Remote/PlayerController.php
@@ -126,15 +126,14 @@ class PlayerController extends AdminController
             return "<a>" . _i('查看游戏记录') . "</a>";
         })->modal(___('gameRecord'), function () {
             $url = "/admin/game-logs?starNO={$this->starNO}&account%5BstarNO%5D={$this->starNO}&account%5BaccountName%5D=&7400aa2d4dc1b92a5020208455da3a0e=&5bb823deeeb717f089b8d2fdf9e1133b=&model=1";
-            modal($url);
-
+            return modal($url);
         });
 
         $grid->column('changeMoney', ___('changeMoney'))->display(function ($time) {
             return "<a>" . _i('金币变化记录') . "</a>";
         })->modal(___('changeMoney'), function () {
             $url = "/admin/change-gold-records?&account%5BstarNO%5D={$this->starNO}&account%5BaccountName%5D=&5fa15899881d341d6f1c374d182f32d3=&a9acc94113d0464afe17ab2c2ee77fbc=&model=1";
-            modal($url);
+            return modal($url);
         });
 
 //        $grid->column('sex', ___('Sex'));
